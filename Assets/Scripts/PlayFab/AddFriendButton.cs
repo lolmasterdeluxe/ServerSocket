7f3988d677fabc6073f4a29fab5d4ff98ee69194
lolmasterdeluxe <kiddrifdi@gmail.com>
2023-01-17 01:52:06 +0800

    improved friend list
    added more friend list functions

---
+++ Assets/Scripts/PlayFab/AddFriendButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AddFriendButton : MonoBehaviour
+{
+    FriendManager friendManager;
+    [SerializeField]
+    private TextMeshProUGUI displayName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        friendManager = FindObjectOfType<FriendManager>();
+    }
+
+    public void OnAddFriend() // To add friend based on display name
+    {
+        friendManager.SendFriendRequest(FriendIdType.DisplayName, displayName.text);
+    }
+}
--- Assets/Scripts/PlayFab/FriendManager.cs
+++ Assets/Scripts/PlayFab/FriendManager.cs
@@ -8,23 +8,36 @@ using UnityEngine.SceneManagement;
 
 public class FriendManager : MonoBehaviour
 {
-    [SerializeField] 
-    private TextMeshProUGUI friendList;
     [SerializeField]
-    private TMP_InputField addFriendText, unfriendText;
+    private TMP_InputField searchFriendInputField, unfriendText;
     private List<FriendInfo> _friends = null;
-
-    public enum FriendIdType { PlayFabId, Username, Email, DisplayName };
+    [SerializeField]
+    private Transform friendListContainer, friendRequestContainer, addFriendContainer;
+    [SerializeField]
+    private GameObject friendListBar, friendRequestBar, addFriendBar, notificationPanel;
 
     public void DisplayFriends(List<FriendInfo> friendsCache)
     {
-        friendList.text = "";
         friendsCache.ForEach(f =>
         {
             Debug.Log(f.FriendPlayFabId + "," + f.TitleDisplayName);
-            friendList.text += f.TitleDisplayName + "\n";
-            if (f.Profile != null)
-                Debug.Log(f.FriendPlayFabId + "/" + f.Profile.DisplayName);
+
+            GameObject friendGO = Instantiate(friendListBar, friendListContainer);
+            TextMeshProUGUI username = friendGO.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI level = friendGO.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI lastOnline = friendGO.transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+
+            username.text = f.TitleDisplayName;
+            level.text = OnGetPlayerLevel(f.FriendPlayFabId);
+            
+            System.DateTime currentDate = System.DateTime.Now;
+            if (currentDate.Date == f.Profile.LastLogin.Value.Date)
+                lastOnline.text = "Last Online: Today";
+            else
+            {
+                double numOfDays = (currentDate - f.Profile.LastLogin.Value).TotalDays;
+                lastOnline.text = "Last Online: " + numOfDays.ToString() + " days ago";
+            }
         });
     }
 
@@ -37,12 +50,15 @@ public class FriendManager : MonoBehaviour
             XboxToken = null
         }, result =>
         {
+            // Refresh list
+            foreach (GameObject GO in friendListContainer)
+                Destroy(GO);
             _friends = result.Friends;
             DisplayFriends(_friends); // Triggers your UI
         }, DebugLogger.Instance.OnPlayfabError);
     }
 
-    private void AddFriend(FriendIdType idType, string friendId)
+    public void SendFriendRequest(FriendIdType idType, string friendId)
     {
         var request = new AddFriendRequest();
         switch (idType)
@@ -63,13 +79,17 @@ public class FriendManager : MonoBehaviour
                 break;
         }
         // Execute request and update friends when we are done
-        PlayFabClientAPI.AddFriend(request, result => { Debug.Log("Friend added successfully!"); }, 
-            DebugLogger.Instance.OnPlayfabError);
-    }
-
-    public void OnAddFriend() // To add friend based on display name
-    {
-        AddFriend(FriendIdType.DisplayName, addFriendText.text);
+        PlayFabClientAPI.AddFriend(request,
+            result =>
+            {
+                DebugLogger.Instance.LogText("Friend added successfully!");
+                Notify("Friend request sent!");
+            },
+            error =>
+            {
+                DebugLogger.Instance.OnPlayfabError(error);
+                Notify("Friend request failed, please try again.");
+            });
     }
 
     // unlike AddFriend, RemoveFriend only takes a PlayFab ID
@@ -101,4 +121,109 @@ public class FriendManager : MonoBehaviour
             Debug.Log("unfriend");
         }, DebugLogger.Instance.OnPlayfabError);
     }
+
+    public void SearchFriend()
+    {
+        var req = new GetAccountInfoRequest
+        {
+            TitleDisplayName = searchFriendInputField.text
+        };
+        PlayFabClientAPI.GetAccountInfo(req
+        , result =>
+        {
+            GameObject searchFriendGO = Instantiate(friendRequestBar, friendListContainer);
+            TextMeshProUGUI username = searchFriendGO.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI level = searchFriendGO.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI lastOnline = searchFriendGO.transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+
+            username.text = result.AccountInfo.TitleInfo.DisplayName;
+            level.text = OnGetPlayerLevel(result.AccountInfo.PlayFabId);
+
+            System.DateTime currentDate = System.DateTime.Now;
+            if (currentDate.Date == result.AccountInfo.TitleInfo.LastLogin.Value.Date)
+                lastOnline.text = "Last Online: Today";
+            else
+            {
+                double numOfDays = (currentDate - result.AccountInfo.TitleInfo.LastLogin.Value.Date).TotalDays;
+                lastOnline.text = "Last Online: " + numOfDays.ToString() + " days ago";
+            }
+        }, error => 
+        {
+            Notify("No account of that name exists");
+            DebugLogger.Instance.OnPlayfabError(error);
+        });
+    }
+
+    public void ListFriendRequests()
+    {
+        var req = new GetAccountInfoRequest
+        {
+            TitleDisplayName = searchFriendInputField.text
+        };
+        PlayFabClientAPI.GetAccountInfo(req
+        , result =>
+        {
+            GameObject searchFriendGO = Instantiate(friendRequestBar, friendListContainer);
+            TextMeshProUGUI username = searchFriendGO.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI level = searchFriendGO.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI lastOnline = searchFriendGO.transform.GetChild(3).GetComponent<TextMeshProUGUI>();
+
+            username.text = result.AccountInfo.TitleInfo.DisplayName;
+            level.text = OnGetPlayerLevel(result.AccountInfo.PlayFabId);
+
+            System.DateTime currentDate = System.DateTime.Now;
+            if (currentDate.Date == result.AccountInfo.TitleInfo.LastLogin.Value.Date)
+                lastOnline.text = "Last Online: Today";
+            else
+            {
+                double numOfDays = (currentDate - result.AccountInfo.TitleInfo.LastLogin.Value.Date).TotalDays;
+                lastOnline.text = "Last Online: " + numOfDays.ToString() + " days ago";
+            }
+        }, error =>
+        {
+            Notify("No account of that name exists");
+            DebugLogger.Instance.OnPlayfabError(error);
+        });
+    }
+
+    public string OnGetPlayerLevel(string playFabID)
+    {
+        string level = "0";
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest()
+        {
+            PlayFabId = PlayerStats.ID,
+            Keys = null
+        }, result => {
+            DebugLogger.Instance.LogText("Got player level:");
+
+            if (result.Data == null || !result.Data.ContainsKey("Level"))
+                DebugLogger.Instance.LogText("No Level");
+            else
+            {
+                level = result.Data["Level"].Value;
+                DebugLogger.Instance.LogText("Level: " + result.Data["Level"].Value);
+            }
+
+        }, (error) => {
+            DebugLogger.Instance.LogText("Got error retrieving user data:");
+            DebugLogger.Instance.LogText(error.GenerateErrorReport());
+        });
+
+        return level;
+    }
+
+    private void Notify(string notificationText)
+    {
+        notificationPanel.SetActive(true);
+        TextMeshProUGUI notiftext = notificationPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        notiftext.text = notificationText;
+    }
 }
+public enum FriendIdType 
+{ 
+    PlayFabId, 
+    Username, 
+    Email, 
+    DisplayName 
+};
+
