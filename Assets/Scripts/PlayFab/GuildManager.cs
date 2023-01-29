e0aa96a1ceea68043bd3c1be2540bdfaac476344
lolmasterdeluxe <kiddrifdi@gmail.com>
2023-01-30 04:01:56 +0800

    creating guild member list

--- Assets/Scripts/PlayFab/GuildManager.cs
+++ Assets/Scripts/PlayFab/GuildManager.cs
@@ -24,9 +24,13 @@ public class GuildManager : MonoBehaviour
 
     public TextMeshProUGUI recruitmentDescription;
 
-    [Header("Guild Bar Prefabs")]
+    [Header("Guild List Prefabs")]
     public GameObject guildBarPrefab;
     public Transform guildBarContainer;
+
+    [Header("Guild Members Prefabs")]
+    public GameObject memberBarPrefab;
+    public Transform memberBarContainer;
     //public readonly HashSet<KeyValuePair<string, string>> EntityGroupPairs = new HashSet<KeyValuePair<string, string>>();
     //public readonly Dictionary<string, string> GroupNameById = new Dictionary<string, string>();
 
@@ -66,20 +70,19 @@ public class GuildManager : MonoBehaviour
     private void ListGroups(PlayFab.GroupsModels.EntityKey entityKey)
     {
         var request = new ListMembershipRequest { Entity = entityKey };
-        PlayFabGroupsAPI.ListMembership(request, OnListGroups, OnSharedError);
-    }
-    private void OnListGroups(ListMembershipResponse response)
-    {
-        var prevRequest = (ListMembershipRequest)response.Request;
+        PlayFabGroupsAPI.ListMembership(new ListMembershipRequest { Entity = entityKey },
+        result => {
+            foreach (var pair in result.Groups)
+            {
+                GameObject guildBar = Instantiate(guildBarPrefab, guildBarContainer);
+                guildBar.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = pair.GroupName;
+                guildBar.GetComponent<GuildInfo>().SetGroupData(pair.GroupName, pair.Group.Id, pair.Group.Type);
 
-        foreach (var pair in response.Groups)
-        {
-            GameObject guildBar = Instantiate(guildBarPrefab, guildBarContainer);
-            guildBar.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = pair.GroupName;
-            guildBar.GetComponent<GuildInfo>().SetGroupData(pair.GroupName, pair.Group.Id, pair.Group.Type);
-
-            DebugLogger.Instance.LogText("Showing group: " + pair.GroupName);
-        }
+                DebugLogger.Instance.LogText("Showing group: " + pair.GroupName);
+            }
+        }, (error) => {
+            DebugLogger.Instance.LogText(error.GenerateErrorReport());
+        });
     }
     #endregion
 
@@ -91,30 +94,36 @@ public class GuildManager : MonoBehaviour
     private void CreateGroup(string groupName, PlayFab.GroupsModels.EntityKey entityKey)
     {
         // A player-controlled entity creates a new group
-        var request = new CreateGroupRequest { GroupName = groupName, Entity = entityKey, CustomTags = new Dictionary<string, string>() { { "Tag", guildTag.text } } };
-        PlayFabGroupsAPI.CreateGroup(request, OnCreateGroup, OnSharedError);
+        PlayFabGroupsAPI.CreateGroup(new CreateGroupRequest { GroupName = groupName, Entity = entityKey, CustomTags = new Dictionary<string, string>() { { "Tag", guildTag.text } } },
+        result => {
+            Debug.Log("Group Created: " + result.GroupName + " - " + result.Group.Id);
+
+            // Setting group data
+            result.AdminRoleId = "ROLE00";
+            result.MemberRoleId = "ROLE03";
+            CreateRole(result.Group, "ROLE00", "Leader");
+            CreateRole(result.Group, "ROLE01", "Co-Leader");
+            CreateRole(result.Group, "ROLE02", "Moderator");
+            CreateRole(result.Group, "ROLE03", "Member");
+
+            SetGroupInfo(new PlayFab.DataModels.EntityKey { Id = result.Group.Id, Type = result.Group.Type });
+            ListGroupsWithParams();
+        }, (error) => {
+            DebugLogger.Instance.LogText(error.GenerateErrorReport());
+        });
     }
 
-    private void OnCreateGroup(CreateGroupResponse response)
-    {
-        Debug.Log("Group Created: " + response.GroupName + " - " + response.Group.Id);
-
-        var prevRequest = (CreateGroupRequest)response.Request;
-        // Setting group data
-        SetGroupInfo(new PlayFab.DataModels.EntityKey { Id = response.Group.Id, Type = response.Group.Type });
-        ListGroupsWithParams();
-    }
     public void DeleteGroup(string groupId)
     {
         // A title, or player-controlled entity with authority to do so, decides to destroy an existing group
-        var request = new DeleteGroupRequest { Group = EntityKeyMaker(groupId) };
-        PlayFabGroupsAPI.DeleteGroup(request, OnDeleteGroup, OnSharedError);
-    }
-    private void OnDeleteGroup(EmptyResponse response)
-    {
-        var prevRequest = (DeleteGroupRequest)response.Request;
-        Debug.Log("Group Deleted: " + prevRequest.Group.Id);
-        ListGroupsWithParams();
+        PlayFabGroupsAPI.DeleteGroup(new DeleteGroupRequest { Group = EntityKeyMaker(groupId) },
+        result => {
+            var prevRequest = (DeleteGroupRequest)result.Request;
+            Debug.Log("Group Deleted: " + prevRequest.Group.Id);
+            ListGroupsWithParams();
+        }, (error) => {
+            DebugLogger.Instance.LogText(error.GenerateErrorReport());
+        });
     }
 
     public void WipeOffTestGroups()
@@ -125,6 +134,56 @@ public class GuildManager : MonoBehaviour
         }
         
     }
+    #endregion
+
+    #region Group Role Management
+    private void CreateRole(PlayFab.GroupsModels.EntityKey entityKey, string roleId, string roleName)
+    {
+        PlayFabGroupsAPI.CreateRole(new CreateGroupRoleRequest() { Group = entityKey, RoleId = roleId, RoleName = roleName },
+        result => {
+            DebugLogger.Instance.LogText(roleId + ": " + roleName + " created successfully");
+        }, (error) => {
+            DebugLogger.Instance.LogText(error.GenerateErrorReport());
+        });
+    }
+
+    private void SetRole(PlayFab.GroupsModels.EntityKey groupEntityKey, PlayFab.GroupsModels.EntityKey memberEntityKey, string roleId)
+    {
+        List<PlayFab.GroupsModels.EntityKey> membersToChange = new List<PlayFab.GroupsModels.EntityKey>() { memberEntityKey };
+        PlayFabGroupsAPI.ChangeMemberRole(new ChangeMemberRoleRequest() 
+        { 
+            Group = groupEntityKey,  
+            Members = membersToChange , 
+            OriginRoleId = "ROLE03",
+            DestinationRoleId = roleId
+        },
+        result => {
+            var prevRequest = (ChangeMemberRoleRequest)result.Request;
+            DebugLogger.Instance.LogText(prevRequest.Members + " changed from " + prevRequest.OriginRoleId + " to " + prevRequest.DestinationRoleId);
+        }, (error) => {
+            DebugLogger.Instance.LogText(error.GenerateErrorReport());
+        });
+    }
+    #endregion
+
+    #region List Group members
+    private void ListGroupMembers(PlayFab.GroupsModels.EntityKey entityKey)
+    {
+        PlayFabGroupsAPI.ListGroupMembers(new ListGroupMembersRequest { Group = entityKey }, 
+        result =>
+        {
+            for (int i = 0;i < result.Members.Count; ++i)
+            {
+                GameObject memberBar = Instantiate(memberBarPrefab, memberBarContainer);
+                memberBar.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = result.Members[i].Members[i].Key.Id;
+            }
+        }
+        , (error) =>
+        {
+
+        });
+    }
+
     #endregion
 
     #region Group data setting / getting
@@ -177,7 +236,6 @@ public class GuildManager : MonoBehaviour
     {
         ObjectResult groupDataResult;
         groupData.TryGetValue("GroupData", out groupDataResult);
-        //groupDataResult = groupData.GetValueOrDefault("GroupData");
         DebugLogger.Instance.LogText(groupDataResult.ToJson());
 
         GroupData groupJsonData = JsonUtility.FromJson<GroupData>(groupDataResult.DataObject.ToString());
@@ -233,14 +291,13 @@ public class GuildManager : MonoBehaviour
     #region Group Administrative Functions
     public void KickMember(string groupId, PlayFab.GroupsModels.EntityKey entityKey)
     {
-        var request = new RemoveMembersRequest { Group = EntityKeyMaker(groupId), Members = new List<PlayFab.GroupsModels.EntityKey> { entityKey } };
-        PlayFabGroupsAPI.RemoveMembers(request, OnKickMembers, OnSharedError);
-    }
-    private void OnKickMembers(EmptyResponse response)
-    {
-        var prevRequest = (RemoveMembersRequest)response.Request;
-
-        Debug.Log("Entity kicked from Group: " + prevRequest.Members[0].Id + " to " + prevRequest.Group.Id);
+        PlayFabGroupsAPI.RemoveMembers(new RemoveMembersRequest { Group = EntityKeyMaker(groupId), Members = new List<PlayFab.GroupsModels.EntityKey> { entityKey } },
+        result => {
+            var prevRequest = (RemoveMembersRequest)result.Request;
+            Debug.Log("Entity kicked from Group: " + prevRequest.Members[0].Id + " to " + prevRequest.Group.Id);
+        }, (error) => {
+            DebugLogger.Instance.LogText(error.GenerateErrorReport());
+        });
     }
     #endregion
 }
